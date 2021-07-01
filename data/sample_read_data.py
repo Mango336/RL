0113a1c594e4503e5aa9c91ad0471b04b9b93d63
Mango336 <ylm245586@gmail.com>
2021-07-01 10:33:58 +0800

    add sarsa

---
+++ data/sample_read_data.py
@@ -0,0 +1,77 @@
+import json, time, datetime, math, csv, copy, sys
+from dateutil.parser import parse
+
+# % read worker attribute: worker_quality
+# 读取worker_quality.csv 中worker_quality属性
+worker_quality = {}
+csvfile = open("worker_quality.csv", "r")
+csvreader = csv.reader(csvfile)
+print("csvreader: {0}".format(csvreader))
+tag = 0
+for line in csvreader:
+    if tag == 0:
+        tag += 1
+        continue
+    if float(line[1]) > 0.0:  # worker_quality > 0.0
+        worker_quality[int(line[0])] = float(line[1]) / 100.0
+csvfile.close()
+
+# print(worker_quality)
+
+# % read project id
+file = open("project_list.csv", "r")
+project_list_lines = file.readlines()
+file.close()
+project_dir = "project/"
+entry_dir = "entry/"
+
+all_begin_time = parse("2018-01-01T0:0:0Z")
+
+project_info = {}
+entry_info = {}
+limit = 24
+industry_list = {}
+index = 1
+list_len = len(project_list_lines)
+for line in project_list_lines:
+    line = line.strip('\n').split(',')
+
+    project_id = int(line[0])
+    entry_count = int(line[1])
+
+    file = open(project_dir + "project_" + str(project_id) + ".txt", "r", encoding="utf-8")
+    htmlcode = file.read()
+    file.close()
+    text = json.loads(htmlcode)
+
+    project_info[project_id] = {}
+    project_info[project_id]["sub_category"] = int(text["sub_category"])  # % project sub_category
+    project_info[project_id]["category"] = int(text["category"])  # % project category
+    project_info[project_id]["entry_count"] = int(text["entry_count"])  # % project answers in total
+    project_info[project_id]["start_date"] = parse(text["start_date"])  # % project start date
+    project_info[project_id]["deadline"] = parse(text["deadline"])  # % project end date
+
+    if text["industry"] not in industry_list:
+        industry_list[text["industry"]] = len(industry_list)
+    project_info[project_id]["industry"] = industry_list[text["industry"]]  # % project domain
+
+    entry_info[project_id] = {}
+    k = 0
+    while (k < entry_count):
+        file = open(entry_dir + "entry_" + str(project_id) + "_" + str(k) + ".txt", "r", encoding="utf-8")
+        htmlcode = file.read()
+        file.close()
+        text = json.loads(htmlcode)
+
+        for item in text["results"]:
+            entry_number = int(item["entry_number"])
+            entry_info[project_id][entry_number] = {}
+            entry_info[project_id][entry_number]["entry_created_at"] = parse(
+                item["entry_created_at"])  # % worker answer_time
+            entry_info[project_id][entry_number]["worker"] = int(item["author"])  # % work_id
+        k += limit
+    index += 1
+    if index % 10 == 0 or index == list_len:
+        print('\r当前读取数据进度：{0}>{1}%'.format('=' * int(index / list_len * 100), int(index / list_len * 100)), end='')
+
+print("\nfinish read_data")
---
+++ data/sarsa.py
@@ -0,0 +1,71 @@
+import numpy as np
+import gym
+# from gridworld 
+from sample_read_data import *
+
+
+class SarsaAgent(object):
+    def __init__(self, obs_n, act_n, learning_rate=0.01, gamma=0.9, e_greed=0.1):
+        self.act_n = act_n  # 动作维度 有几个动作可选
+        self.lr = learning_rate  # 学习率
+        self.gamma = gamma  # reward的衰减率
+        self.epsilon = e_greed  # 按一定概率随机选动作
+        self.Q = np.zeros((obs_n, act_n))
+
+    # 根据输入观察值 采样输出的动作值
+    def sample(self, obs):
+        if np.random.uniform(0, 1) < (1.0 - self.epsilon):  # 根据table的Q值选动作  uniform(0, 1)=>[0, 1]的随机数
+            action = self.predict(obs)  # 拿到最优动作
+        else:
+            action = np.random.choice(self.act_n)  # 其他动作也有一定概率随机探索到
+        return action
+
+    # 根据输入观察值 预测输出的动作值
+    def predict(self, obs):
+        Q_list = self.Q[obs, :]  # Q_list中 行代表状态 列代表动作
+        maxQ = np.max(Q_list)  # 贪心策略
+        action_list = np.where(Q_list == maxQ)[0]  # maxQ可能对应多个action
+        action = np.random.choice(action_list)  # 从这多个action中随机选取一个
+        return action
+
+    def learn(self, obs, action, reward, next_obs, next_action, done):
+        """
+        obs: 交互前的obs s_t
+        action: 本次交互选择的action a_t
+        reward: 本次动作获得的奖励 r
+        next_obs: 本次交互后的obs s_t+1
+        next_action: 根据当前Q表格 针对next_obs会选择的动作 即：a_t+1
+        done: episode是否介绍
+        """
+        predict_Q = self.Q[obs, action]  # 预测到的下一个Q值
+        if done:  # 最后一轮迭代  没有下一个状态
+            target_Q = reward
+        else:
+            target_Q = reward + self.gamma * self.Q[next_obs, next_action]  # Sarsa
+        self.Q[obs, action] += self.lr * (target_Q - predict_Q)
+
+def run_episode(env, agent, render=False):
+    total_steps = 0  # 记录每个episode走了多少step
+    total_reward = 0
+    obs = env.reset()  # 重置环境 重新开始一个新的episode
+    action = agent.sample(obs)  # agent根据算法选择一个动作输出
+    while True:
+        next_obs, reward, done, _ = env.step(action)  # 与环境进行一个交互
+        next_action = agent.sample(next_obs)  # 根据算法选择一个动作
+        agent.learn(obs, action, reward, next_obs, next_action, done)  # 训练Sarsa
+
+        action = next_action
+        obs = next_obs  # 存储上一个观察值
+        total_reward += reward
+        total_steps += 1  # 计算step数
+        if render:
+            env.render()  # 渲染新的一帧图形
+        if done:
+            break
+    return total_reward, total_steps
+
+# if __name__ == "__main__":
+#     obs = env.reset()
+#     action = agent.sample(obs)
+#     while True:
+#
